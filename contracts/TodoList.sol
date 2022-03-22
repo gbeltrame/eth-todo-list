b54c8f80ebd200e1fce7977d8e001faf0dca24a9
gbeltrame <beltramegustavo87@gmail.com>
2022-03-21 23:47:17 -0300

    Added web client connectivity to blockchain

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,22 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    constructor() public {
+        createTask("Check out dappuniversity.com");
+    }
+
+    mapping(uint => Task) public tasks;
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount,_content,false);
+
+    }
 }
