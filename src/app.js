38825c5303476800e59847e235873a6bd1a85a79
gbeltrame <beltramegustavo87@gmail.com>
2022-03-24 21:11:49 -0300

    Display Tasks from blockchain on web app

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ App = {
     load: async () => {
       await App.loadWeb3()
       await App.loadAccount()
+      await App.loadContract()
+      await App.render()
     },
   
     // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
@@ -37,17 +39,90 @@ App = {
       // Non-dapp browsers...
       else {
         console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+
+  },
+
+  loadAccount: async () => {
+    // Set the current blockchain account
+    App.account = web3.eth.accounts[0];
+    console.log(App.account);
+  },
+
+  loadContract: async () => {
+    // Create a Javascript version of the smart contract
+    const todoList = await $.getJSON('TodoList.json')
+    App.contracts.TodoList = TruffleContract(todoList)
+    App.contracts.TodoList.setProvider(App.web3Provider)
+    
+    // Hydrate the smart contract with values from the blockchain
+    App.todoList = await App.contracts.TodoList.deployed()
+  },
+
+  render: async() => {
+    //Prevent double render
+    if(App.loading){
+      return
+    }
+
+    //Update loading state
+    App.setLoading(true)
+    
+    
+    //Render Account
+    $('#account').html(App.account)
+
+    //Render tasks
+    await App.renderTasks()
+
+    //Update loading state
+    App.setLoading(false)
+    
+  },
+
+  renderTasks: async() => {
+    //Load tasks from the blockchain
+    const taskCount = await App.todoList.taskCount()
+    const $taskTemplate = $('.taskTemplate')
+    //Render out tasks with new task template
+    for(var i = 1; i<= taskCount; i++)
+    {
+      const task = await App.todoList.tasks(i)
+      const taskId = task[0].toNumber()
+      const taskContent = task[1]
+      const taskCompleted = task[2]
+
+      //Create HTML for the task
+      const $newTaskTemplate = $taskTemplate.clone()
+      $newTaskTemplate.find('.content').html(taskContent)
+      $newTaskTemplate.find('input')
+                      .prop('name',taskId)
+                      .prop('checked', taskCompleted)
+                      //.on('click',App.toggleCompleted)
+
+      if(taskCompleted){
+        $('#completedTaskList').append($newTaskTemplate)
+      } else {
+        $('#taskList').append($newTaskTemplate)
       }
-    },
-  
-    loadAccount: async () => {
-      // Set the current blockchain account
-      App.account = web3.eth.accounts[0];
-      console.log(App.account);
-    },
+      //Show tasks
+      $newTaskTemplate.show()
+    }
+  },
 
+  setLoading: (boolean) => {
+    App.loading = boolean
+    const loader = $('#loader')
+    const content = $('#content')
+    if(boolean){
+      loader.show()
+      content.hide()
+    } else {
+      loader.hide()
+      content.show()
+    }
+  }
 }
-
 $(() => {
     $(window).load(() => {
         App.load()
